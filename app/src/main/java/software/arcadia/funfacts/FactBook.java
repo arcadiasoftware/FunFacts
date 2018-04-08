7d9f7b0cfc74212a9ca74b40f492bcc4574ded96
Craig Booker <craig@arcadia.software>
2018-04-07 20:55:12 -0500

    Created blank FactBook class.

---
+++ app/src/main/java/software/arcadia/funfacts/FactBook.java
@@ -0,0 +1,16 @@
+package software.arcadia.funfacts;
+
+/*
+ * Created By Craig Booker on 04/07/2018
+ *
+ */
+
+public class FactBook {
+    // Fields or member variables - properties about the object.
+
+    // Methods - actions the object can take.
+    public String getFact() {
+
+    }
+
+}
